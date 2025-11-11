6aa18306f3f2d302bd98237cf5806008d693c663
kotosmiornica <145379435+kotosmiornica@users.noreply.github.com>
2025-11-11 15:12:15 +0900

    wee

--- scripts/shootyou.gd
+++ scripts/shootyou.gd
@@ -5,20 +5,11 @@ var bullet_scene = preload("res://scenes/testbullet.tscn")
 @onready var pivot = $weapon/Sprite2D/pivot
 @onready var bang = $Bang
 
-@export var fire_rate := 0.15
+@export var fire_rate := 0.12
 var can_fire := true
 
 func _physics_process(_delta: float) -> void:
-	var dir := Vector2.ZERO
-
-	if Input.is_action_pressed("cursor_right"):
-		dir.x += 1
-	if Input.is_action_pressed("cursor_left"):
-		dir.x -= 1
-	if Input.is_action_pressed("cursor_down"):
-		dir.y += 1
-	if Input.is_action_pressed("cursor_up"):
-		dir.y -= 1
+	var dir := Input.get_vector("cursor_left", "cursor_right", "cursor_up", "cursor_down")
 
 	if dir != Vector2.ZERO and can_fire:
 		rotation = dir.angle()
